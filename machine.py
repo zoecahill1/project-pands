c4c6e724bb4bb0b1891cf72d11feab4c8af01ad6
Zoe <zcahill90@gmail.com>
2019-04-19 01:17:29 +0100

    added decision tree code

---
+++ machine.py
@@ -0,0 +1,43 @@
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+from sklearn.datasets import load_iris
+import seaborn as sns
+from graphviz import Source
+
+def decisionTree():
+    iris = sns.load_dataset("iris")
+    y = iris.species
+    X = iris.drop('species', axis=1)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size = 0.3, 
+                                                        random_state = 100, 
+                                                        stratify = y)
+
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X_train,y_train)
+
+    #DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,max_features=None, max_leaf_nodes=None,min_impurity_split=1e-07, min_samples_leaf=1,min_samples_split=2, min_weight_fraction_leaf=0.0,presort=False, random_state=None, splitter='best')
+
+    iris2 = load_iris()
+
+    #tree.export_graphviz(clf, 
+    #out_file='res/iris.dot', feature_names=iris2.feature_names, 
+                                                #class_names=iris2.target_names, 
+                                                #filled=True, 
+                                                #rounded=True, 
+                                                #special_characters=True)
+
+    y_pred = (clf.predict(X_test))
+
+    print ('Accuracy Score')
+    print (accuracy_score(y_test, y_pred)* 100)
+
+def view():
+    path = 'res/iris.dot'
+    s = Source.from_file(path)
+    s.view()
+
+
--- start.py
+++ start.py
@@ -1,11 +1,14 @@
 import analysis
+import machine
 
 def main():
     # To change function look at function descriptions in analysis.py
     # decide which one you would like to run
     # and input in the form analysis.functionName()
     #analysis.readData()
-    analysis.scatterPetal()
+    #analysis.scatterPetal()
+
+    machine.decisionTree()
 
 if __name__ == "__main__":
     main()
